115f1cbdf6007dab6e1b4312097917ff9c8b509d
Saswat Pal <saswat2001@gmail.com>
2026-05-11 23:46:05 +0530

    chore(usage-service): initialize project setup

---
+++ usage-service/src/test/java/com/homewatt/usage_service/UsageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homewatt.usage_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

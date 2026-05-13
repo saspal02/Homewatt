1bb2c1391f5a48ab2bec3bdfba7afabc294f8a0f
Saswat Pal <saswat2001@gmail.com>
2026-05-13 21:35:51 +0530

    Implement alert service

---
+++ alert-service/src/main/java/com/homewatt/alert_service/entity/Alert.java
@@ -0,0 +1,26 @@
+package com.homewatt.alert_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "alert")
+public class Alert {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private LocalDateTime createdAt;
+    private boolean sent;
+
+}
